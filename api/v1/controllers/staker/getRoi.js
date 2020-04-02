47797d43cc53836b6889117965264c8e7308718e
devintegral4 <devintegral4@sfxdx.ru>
2020-04-02 04:45:31 +0300

    some more fixes

--- api/v1/controllers/staker/getRoi.js
+++ api/v1/controllers/staker/getRoi.js
@@ -5,48 +5,47 @@ const BN = Web3.utils.BN;
 const {calcValidatorRewards, getEpochSnapshot, getCurrentEpoch, stakers} = require('../../../../mixins/contractsMethods')
 const zeroInt = new BN(0);
 
-const ErrEpochNumIsTooBig = "current epoch is less than number of epochs requested for calculation";
-const ErrMissingRequestParameters = (req) => `Missing request parameters: id: ${req.id}, epochs: ${req.epochsNum}`;
+const errEpochNumIsTooBig = "current epoch is less than number of epochs requested for calculation";
+const errMissingRequestParameters = (req) => `Missing request parameters: id: ${req.id}, epochs: ${req.epochsNum}`;
 
 module.exports = async (req, res, next) => {
-  if (!req.params.id || !req.params.epochsNum) {
-      throw ErrMissingRequestParameters(req);
-  }
-
-  const second = 1;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-  const year = day * 365;
-
-  // we have to ensure that epochsNum is represented as string
-  let epochNumber = req.params.epochsNum.toString();
-  let epochNum = new BN(epochNumber);
-  let stkrId = req.params.id.toString();
-  let currentEpochRes = await getCurrentEpoch();
-  let currentEpoch = new BN(currentEpochRes);
-  if (currentEpoch.sub(epochNum).cmp(zeroInt) === -1) {
-      throw ErrEpochNumIsTooBig;
-  }
-
-  let epochNumInt = parseInt(epochNumber);
-  let calcRewardsStartEpoch = new BN(currentEpoch).sub(epochNum);
-  let rewards = await calcValidatorRewards(stkrId, calcRewardsStartEpoch, epochNum);
-  let totalDuration = new BN(0);
-  for (let i=0; i < epochNumInt; i++) {
-      let targetEpoch = currentEpoch.sub(new BN(i+1));
-      let snapshot = await getEpochSnapshot(targetEpoch);
-      totalDuration = totalDuration.add(new BN(snapshot.duration));
-  }
-
-  let staker = await stakers(stkrId);
-  let stake = new BN(staker.stakeAmount);
-  let reward = new BN(rewards[0]);
-  let rewardYearScale = reward.mul(new BN(year));
-  let rewardPerYear = rewardYearScale.div(totalDuration);
-  let roi = rewardPerYear.div(stake);
-  
   try {          
+    if (!req.params.id || !req.params.epochsNum) {
+        throw errMissingRequestParameters(req);
+    }
+  
+    const second = 1;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+    const year = day * 365;
+  
+    // we have to ensure that epochsNum is represented as string
+    let epochNumber = req.params.epochsNum.toString();
+    let epochNum = new BN(epochNumber);
+    let stkrId = req.params.id.toString();
+    let currentEpochRes = await getCurrentEpoch();
+    let currentEpoch = new BN(currentEpochRes);
+    if (currentEpoch.sub(epochNum).cmp(zeroInt) === -1) {
+        throw errEpochNumIsTooBig;
+    }
+  
+    let epochNumInt = parseInt(epochNumber);
+    let calcRewardsStartEpoch = new BN(currentEpoch).sub(epochNum);
+    let rewards = await calcValidatorRewards(stkrId, calcRewardsStartEpoch, epochNum);
+    let totalDuration = new BN(0);
+    for (let i=0; i < epochNumInt; i++) {
+        let targetEpoch = currentEpoch.sub(new BN(i+1));
+        let snapshot = await getEpochSnapshot(targetEpoch);
+        totalDuration = totalDuration.add(new BN(snapshot.duration));
+    }
+  
+    let staker = await stakers(stkrId);
+    let stake = new BN(staker.stakeAmount);
+    let reward = new BN(rewards[0]);
+    let rewardYearScale = reward.mul(new BN(year));
+    let rewardPerYear = rewardYearScale.div(totalDuration);
+    let roi = rewardPerYear.div(stake);
     return res.json(okResp({ roi: roi.toString() }));
   }
   catch (err){
--- mixins/web3.js
+++ mixins/web3.js
@@ -1,7 +1,6 @@
 const config = require(`config`);
 const Web3 = require('web3');
 
-console.log("config.rpcPort", config.rpcPort);
 module.exports = new Web3(
     new Web3.providers.WebsocketProvider(`ws://${config.nodeAddr}:${config.wsPort}`)
 );
